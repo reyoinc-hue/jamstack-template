fba1dc710189c979c79b6c4a0693334f103a2683
Dug Mini <reyoinc@gmail.com>
2025-08-21 23:24:46 -0700

    chore: add sitemap integration and site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://reyoinc-hue.github.io/jamstack-template', // ?? update later if you use a custom domain
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
